411c4aa5301c7544552e1a799935e8a2113f267c
mpark2021 <mpark2021@gmail.com>
2019-11-02 21:08:16 +0900

    Modified Ver1 according to BaseModel

--- Model/Ver1.py
+++ Model/Ver1.py
@@ -1,12 +1,14 @@
 import keras
+from Model.BaseModel import BaseModel
 
-model = keras.Sequential(
-    [
-        keras.layers.Dense(32, input_dim=48 * 4, activation="sigmoid"),
-        keras.layers.Dense(64, activation="sigmoid"),
-        keras.layers.Dense(48, activation="sigmoid")
-    ]
-)
 
-model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-
+class Model_Version1(BaseModel):
+    def define_model(self) -> keras.Model:
+        model = keras.Sequential(
+            [
+                keras.layers.Dense(32, input_dim=48 * 4, activation="sigmoid"),
+                keras.layers.Dense(64, activation="sigmoid"),
+                keras.layers.Dense(48, activation="sigmoid")
+            ]
+        )
+        return model
