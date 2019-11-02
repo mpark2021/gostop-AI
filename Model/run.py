56e596daf98659343e8684e6717b05e68ada3f78
mpark2021 <mpark2021@gmail.com>
2019-11-02 18:55:44 +0900

    Modified code "run" - model save

--- Model/run.py
+++ Model/run.py
@@ -1,9 +1,19 @@
 from Model.Ver1 import model
 from Model.utils import *
+import os
 
 model.summary()
 x_train, y_train, x_eval, y_eval = parse("../Game/version1")
+print(x_train)
+print(y_eval)
 model.fit(x_train, y_train, epochs=100)
 
 result = model.evaluate(x_eval, y_eval)
 print(result)
+
+
+if not os.path.exists("Version1"):
+    os.mkdir("Version1")
+
+model.save("Version1/model")
+model.save_weights("Version1/weights")
