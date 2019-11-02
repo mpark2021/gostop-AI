b63d20de65f8a0a0fa9ccaea9d23690a3a7b4f51
mpark2021 <mpark2021@gmail.com>
2019-11-02 21:07:04 +0900

    Simple forward example using random data

--- Model/SimpleForward.py
+++ Model/SimpleForward.py
@@ -1,11 +1,21 @@
 import keras
 
 def forward(model_path, weights_path, data):
-    with open(model_path, "r") as f:
-        json = f.read()
-        model = keras.models.model_from_json(json)
+        model = keras.models.load_model(model_path)
         model.compile(optimizer="adam",
               loss="sparse_categorical_crossentropy",
               metrics=["accuracy"])
         model.load_weights(weights_path)
         return model(data)
+
+
+if __name__ =="__main__":
+    import numpy as np
+    import tensorflow as tf
+    data = np.random.random((1, 48*4))
+    data = data.round()
+    x = tf.convert_to_tensor(data, dtype=tf.float32)
+    y = forward("Version1/Model", "Version1/Weight", x)
+    print(y)
+    value = keras.backend.eval(y)
+    print(value * np.split(data, [48,], 1)[0])
