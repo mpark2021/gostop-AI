70c6f3be9e4ae8ae7153e4f9f34d4fcef0a0cda5
mpark2021 <mpark2021@gmail.com>
2019-11-09 18:11:05 +0900

    Rename "Model_Version 1" to "Model"

--- Model/run.py
+++ Model/run.py
@@ -1,9 +1,9 @@
-from Model.Ver1 import Model_Version1
+from Model.Version1 import Model
 from Model.utils import *
 import os
 
 x_train, y_train, x_eval, y_eval = parse("../Game/version1")
-model = Model_Version1()
+model = Model()
 m = model.run(x_train, y_train, x_eval, y_eval)
 
 m.save("Version1/Model", include_optimizer=False)
