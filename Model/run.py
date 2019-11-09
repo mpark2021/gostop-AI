033211c513fb96c5625b3230bbf9d70dc062a87a
mpark2021 <mpark2021@gmail.com>
2019-11-09 18:38:15 +0900

    Add "version"

--- Model/run.py
+++ Model/run.py
@@ -2,9 +2,19 @@ from Model.Version1 import Model
 from Model.utils import *
 import os
 
-x_train, y_train, x_eval, y_eval = parse("../Game/version1")
+x_train, y_train, x_eval, y_eval = parse("../Game/Version0")
 model = Model()
 m = model.run(x_train, y_train, x_eval, y_eval)
 
-m.save("Version1/Model", include_optimizer=False)
-m.save_weights("Version1/Weight")
+generation = 1
+version_folder_name = "Version1"
+folder_name = "Generation" + str(generation)
+try:
+    os.mkdir(version_folder_name)
+    os.mkdir("Version1/" + folder_name)
+
+    m.save("Version1/" + folder_name + "/Model", include_optimizer=False)
+    m.save_weights("Version1/" + folder_name + "/Weight")
+except:
+    print("Failed to make new directory")
+
