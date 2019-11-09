8141cd40383197b6edfbc0f972c03f6ef72244f1
mpark2021 <mpark2021@gmail.com>
2019-11-09 20:52:36 +0900

    save version folder

--- Model/run.py
+++ Model/run.py
@@ -2,7 +2,7 @@ from Model.Version1 import Model
 from Model.utils import *
 import os
 
-x_train, y_train, x_eval, y_eval = parse("../Game/Version0")
+x_train, y_train, x_eval, y_eval = parse("../Game/Version1/Generation0")
 model = Model()
 m = model.run(x_train, y_train, x_eval, y_eval)
 
@@ -10,11 +10,13 @@ generation = 1
 version_folder_name = "Version1"
 folder_name = "Generation" + str(generation)
 try:
-    os.mkdir(version_folder_name)
-    os.mkdir("Version1/" + folder_name)
+    if not os.path.exists(version_folder_name):
+        os.mkdir(version_folder_name)
+    if not os.path.exists(version_folder_name + "/" + folder_name):
+        os.mkdir(version_folder_name + "/" + folder_name)
 
-    m.save("Version1/" + folder_name + "/Model", include_optimizer=False)
-    m.save_weights("Version1/" + folder_name + "/Weight")
-except:
+    m.save(version_folder_name + "/" + folder_name + "/Model", include_optimizer=False)
+    m.save_weights(version_folder_name + "/" + folder_name + "/Weights")
+except OSError:
     print("Failed to make new directory")
 
