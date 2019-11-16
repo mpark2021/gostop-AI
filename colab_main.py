46bdbfcd3fac1e16e7f55f10dd1b2f245a18a07b
mpark2021 <mpark2021@gmail.com>
2019-11-16 18:34:30 +0900

    Colab Main Argc

--- colab_main.py
+++ colab_main.py
@@ -4,3 +4,16 @@ import sys
 sys.path.insert(0, '/content/gdrive/My Drive/Colab Notebooks/')
 
 import Model.run
+
+
+def main():
+    argc = len(sys.argv)
+    if argc == 1:
+        print("Usage")
+        print("[G|Game|g|game version generation num_iter")
+        print("[M||m|model]|Model] version generation epoch=1000")
+
+
+if __name__ =="__main__":
+    main()
+        
