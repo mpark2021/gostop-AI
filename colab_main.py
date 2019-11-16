6a1f4f8d1c9edd696ae25eff9de80566bac76e35
mpark2021 <mpark2021@gmail.com>
2019-11-16 18:56:46 +0900

    Game + Mode + Num_Iter in Colab_main

--- colab_main.py
+++ colab_main.py
@@ -6,10 +6,42 @@ sys.path.insert(0, '/content/gdrive/My Drive/Colab Notebooks/')
 
 def main():
     argc = len(sys.argv)
-    if argc == 1:
+    if argc < 4:
         print("Usage")
         print("[G|Game|g|game] version generation num_iter")
         print("[M|Mode|m|mode] version generation epoch=1000")
+        return
+
+    version = int(sys.argv[2])
+    generation = int(sys.argv[3])
+
+    if sys.argv[1] in ["G", "Game", "g", "game"]:
+        from Game.game import Game
+
+        version_path = "Game/Version%d" % version
+        generation_path = "Game/Generation%d" % generation
+
+        try:
+            if not os.path.exists(version_path):
+                os.makedirs(version_path)
+            os.chdir(version_path)
+
+            if not os.path.exists(generation_path):
+                os.makedirs(generation_path)
+            os.chdir(generation_path)
+
+        except OSError:
+            print("Failed to create directory")
+            exit(-1)
+
+        game = Game(False, version, generation)
+        if len(sys.argv) == 5:
+            num_iter = int(sys.argv[4])
+        else:
+            num_iter = 1000
+        game.run_with_encode(num_iter)
+
+        
 
 
 if __name__ =="__main__":
